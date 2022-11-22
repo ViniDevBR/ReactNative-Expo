c35bf91f73c54b77af8c9038a4fc43d97a8441f9
ViniDevBR <vinigtr386@gmail.com>
2022-11-22 11:47:32 -0300

    :fire: Types

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined;
+      Location: undefined;
+      User: undefined;
+      Add: undefined;
+      Vagas: undefined
+    }
+  }
+}
