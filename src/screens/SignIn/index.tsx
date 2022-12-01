861479a961ea4b57c7e2e716708e9602c7165212
ViniDevBR <vinigtr386@gmail.com>
2022-11-30 21:48:35 -0300

    :fire: Doing screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,14 @@
+import { Text, View } from 'react-native'
 import { Teste } from './styles'
-
+import { Input } from '../../components/Input'
 export function SignIn() {
-  return <Teste />
+  return (
+    <View>
+      <Text>
+        Sou <Text>Junior</Text>
+      </Text>
+
+      <Input placeholder=''/>
+    </View>
+  )
 }
