1a525e8d66b20826ced49bdd5730b04b6f82edd3
ViniDevBR <vinigtr386@gmail.com>
2022-12-01 19:09:31 -0300

    :fire: New styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,50 @@
-import { ReactNode } from 'react'
-import { TextInput, TextInputProps } from 'react-native'
-import { InputContainer } from './styles'
+//REACT
+import { ComponentProps, ReactNode, useState } from 'react'
+import { TextInputProps } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+//STYLES
+import { InputContainer, InputText, IconContainer } from './styles'
+//HOOK FORM
+import { Control, Controller } from 'react-hook-form'
+import { IFormInputs } from '../../screens/SignIn'
+import { useTheme } from 'styled-components'
 
-interface IInput extends TextInputProps {
-  placeholder: string
-  icon?: ReactNode
+export interface InputProps extends TextInputProps {
+  icon?: ComponentProps<typeof Feather>['name']
+  name: string
+  value?: string
 }
 
-export function Input({ ...props }: IInput) {
+export function Input({ ...props }: InputProps) {
+  const { COLORS } = useTheme()
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+
+  function handleInputFocus() {
+    setIsFocused(true)
+  }
+  function handleInputBlur() {
+    setIsFocused(false)
+    setIsFilled(!!props.value)
+  }
+
   return (
-    <InputContainer>
-      <TextInput {...props} />
-      {props.icon}
+    <InputContainer isFocused={isFocused}>
+      <InputText
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        isFocused={isFocused}
+        value={props.value}
+        {...props}
+      />
+
+      <IconContainer>
+        <Feather
+          name={props.icon}
+          size={24}
+          color={isFocused || isFilled ? COLORS.PRIMARY_900 : COLORS.ICON}
+        />
+      </IconContainer>
     </InputContainer>
   )
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,15 +1,39 @@
-import styled from "styled-components/native";
+import { TextInput } from 'react-native';
+import styled, { css } from 'styled-components/native';
 
-export const InputContainer = styled.View`
+interface Props {
+  isFocused: boolean;
+}
+
+
+export const InputContainer = styled.View<Props>`
   background-color: ${props => props.theme.COLORS.BACKGROUND2};
   width: 100%;
   padding: 17px 20px ;
   margin-bottom: 18px;
   border-width: 1px;
   border-radius: 4px;
-  border-color: ${props => props.theme.COLORS.BORDER2};
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  border-color: ${props => props.theme.COLORS.BORDER2};
+
+  ${({ isFocused }) => isFocused && css`
+    border-width: 2px;
+    border-color: ${props => props.theme.COLORS.PRIMARY_900};    
+  `};
 `
 
+export const IconContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-right: 2px;
+  background-color: #FFFFFF;
+`;
+
+
+export const InputText = styled(TextInput) <Props>`
+  flex: 1;
+  background-color: #FFFFFF;
+  color: ${props => props.theme.COLORS.PLACEHOLDER}
+`;
