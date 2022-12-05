0f49cda0b5d35f8aa2ed71b5ee93cfacb7b7aebf
ViniDevBR <vinigtr386@gmail.com>
2022-12-04 22:31:29 -0300

    :fire: New styles

--- src/components/JobCard/index.tsx
+++ src/components/JobCard/index.tsx
@@ -1,9 +1,37 @@
-import { Container } from './styles'
+import { Container, Img, Infos, Time, InfosDetailed } from './styles'
+import JobCardImg from '../../assets/JobCard.png'
 
-export function JobCard() {
+ export interface Props {
+  time: string
+  title: string
+  subtitle: string
+  location: string
+  level: string
+  img: string
+}
+
+export function JobCard(props: Props) {
   return (
     <Container>
-      
+      <Img source={JobCardImg}/>
+
+      <Infos>
+        <InfosDetailed type='title'>
+          UX Designer Junior
+        </InfosDetailed>
+        <InfosDetailed type='subtitle'>
+          Empresa Verde
+        </InfosDetailed>
+        <InfosDetailed type='location'>
+          Bento Gonçalves, Rio Grande do Sul, Brasil
+        </InfosDetailed>
+        <InfosDetailed type='level'> 
+          REMOTO - JUNIOR - PJ
+        </InfosDetailed>
+      </Infos>
+      <Time>
+        Há 6h
+      </Time>
     </Container>
   )
 }
--- src/components/JobCard/styles.ts
+++ src/components/JobCard/styles.ts
@@ -1,3 +1,34 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
-export const Container = styled.View``
+type TInfos = {
+  type: 'title' | 'subtitle' | 'location' | 'level'
+}
+
+export const Container = styled.View`
+  padding: 14px;
+`
+
+export const Img = styled.Image``
+
+export const Infos = styled.View``
+
+export const InfosDetailed = styled.Text<TInfos>`
+
+
+  ${props => props.type === 'title' && css`
+    
+  `}
+  ${props => props.type === 'subtitle' && css`
+
+  `}
+  ${props => props.type === 'location' && css`
+
+  `}
+  ${props => props.type === 'level' && css`
+
+  `}
+`
+
+export const Time = styled.View`
+  font-size: ${props => props.theme.FONT_SIZE.NORMAL};
+`
