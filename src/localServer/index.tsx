7f853122857426e4524f39cb1669191b5d67db22
ViniDevBR <vinigtr386@gmail.com>
2022-12-27 23:46:59 -0300

    :fire: File with all url's

---
+++ src/localServer/index.tsx
@@ -0,0 +1,5 @@
+export const urlVini = 'http://192.168.0.4:4000'
+export const urlRafa = 'http://192.168.0.9:4000'
+export const urlThatto = 'http://192.168.0.9:4000'
+
+//É necessario trocar na tela de Vagas dentro do useFocusEffect (linha 80) e na tela de DetailsVaga (linha 47)
